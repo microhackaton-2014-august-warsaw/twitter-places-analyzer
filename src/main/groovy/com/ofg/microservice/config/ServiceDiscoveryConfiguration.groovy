2ac4cc50a9cfced528fad287107aa9ce6354b1fb
Marcin Jasion <marcinjasion@gmail.com>
2014-08-09 12:55:17 +0200

    ServiceDiscoveryConfiguration.groovy in development

--- src/main/groovy/com/ofg/microservice/config/ServiceDiscoveryConfiguration.groovy
+++ src/main/groovy/com/ofg/microservice/config/ServiceDiscoveryConfiguration.groovy
@@ -1,15 +1,17 @@
 package com.ofg.microservice.config
+
 import com.ofg.infrastructure.discovery.ServiceResolverConfiguration
 import groovy.transform.TypeChecked
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Profile
 
+import static com.ofg.microservice.Profiles.DEVELOPMENT
 import static com.ofg.microservice.Profiles.PRODUCTION
 
 @TypeChecked
 @Configuration
 @Import(ServiceResolverConfiguration)
-@Profile(PRODUCTION)
+@Profile([PRODUCTION, DEVELOPMENT])
 class ServiceDiscoveryConfiguration {
 }
